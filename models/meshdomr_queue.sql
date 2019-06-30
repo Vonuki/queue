631e62db60866ee285bd226ef5cd3f195580e296
Dmitrii K <vonuki@gmail.com>
2019-06-30 03:08:58 +0300

    rm db.php

---
+++ models/meshdomr_queue.sql
@@ -0,0 +1,365 @@
+-- phpMyAdmin SQL Dump
+-- version 
+-- http://www.phpmyadmin.net
+--
+-- Host: meshdomr.mysql
+-- Generation Time: Jun 29, 2019 at 05:44 PM
+-- Server version: 5.6.41-84.1
+-- PHP Version: 5.6.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `meshdomr_queue`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Clients`
+--
+-- Creation: Jun 29, 2019 at 01:25 PM
+--
+
+DROP TABLE IF EXISTS `Clients`;
+CREATE TABLE IF NOT EXISTS `Clients` (
+  `idClient` int(11) NOT NULL,
+  `Description` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `idPerson` int(11) NOT NULL,
+  `Status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Items`
+--
+-- Creation: Jun 24, 2019 at 08:27 PM
+--
+
+DROP TABLE IF EXISTS `Items`;
+CREATE TABLE IF NOT EXISTS `Items` (
+  `idItem` int(11) NOT NULL,
+  `idQueue` int(11) NOT NULL,
+  `idClient` int(11) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `CreateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `StatusDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `RestTime` time NOT NULL,
+  `Position` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migration`
+--
+-- Creation: Jun 25, 2019 at 08:52 PM
+--
+
+DROP TABLE IF EXISTS `migration`;
+CREATE TABLE IF NOT EXISTS `migration` (
+  `version` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `apply_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1561495920),
+('m140209_132017_init', 1561495940),
+('m140403_174025_create_account_table', 1561495940),
+('m140504_113157_update_tables', 1561495941),
+('m140504_130429_create_token_table', 1561495941),
+('m140830_171933_fix_ip_field', 1561495941),
+('m140830_172703_change_account_table_name', 1561495941),
+('m141222_110026_update_ip_field', 1561495941),
+('m141222_135246_alter_username_length', 1561495941),
+('m150614_103145_update_social_account_table', 1561495941),
+('m150623_212711_fix_username_notnull', 1561495941),
+('m151218_234654_add_timezone_to_profile', 1561495941),
+('m160929_103127_add_last_login_at_to_user_table', 1561495941);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Owners`
+--
+-- Creation: Jun 29, 2019 at 01:25 PM
+--
+
+DROP TABLE IF EXISTS `Owners`;
+CREATE TABLE IF NOT EXISTS `Owners` (
+  `idOwner` int(11) NOT NULL,
+  `Description` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `idPerson` int(11) NOT NULL,
+  `Status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+-- Creation: Jun 25, 2019 at 08:52 PM
+--
+
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE IF NOT EXISTS `profile` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `public_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_id` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bio` text COLLATE utf8_unicode_ci,
+  `timezone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`user_id`, `name`, `public_email`, `gravatar_email`, `gravatar_id`, `location`, `website`, `bio`, `timezone`) VALUES
+(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Queues`
+--
+-- Creation: Jun 25, 2019 at 01:32 PM
+--
+
+DROP TABLE IF EXISTS `Queues`;
+CREATE TABLE IF NOT EXISTS `Queues` (
+  `idQueue` int(11) NOT NULL,
+  `Description` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `QueueShare` int(11) NOT NULL,
+  `idOwner` int(11) NOT NULL,
+  `FirstItem` int(11) NOT NULL,
+  `QueueLen` int(11) NOT NULL,
+  `Status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_account`
+--
+-- Creation: Jun 25, 2019 at 08:52 PM
+--
+
+DROP TABLE IF EXISTS `social_account`;
+CREATE TABLE IF NOT EXISTS `social_account` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `provider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `client_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci,
+  `code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `token`
+--
+-- Creation: Jun 25, 2019 at 08:52 PM
+--
+
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE IF NOT EXISTS `token` (
+  `user_id` int(11) NOT NULL,
+  `code` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `type` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Creation: Jun 25, 2019 at 08:52 PM
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `confirmed_at` int(11) DEFAULT NULL,
+  `unconfirmed_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `blocked_at` int(11) DEFAULT NULL,
+  `registration_ip` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `flags` int(11) NOT NULL DEFAULT '0',
+  `last_login_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `last_login_at`) VALUES
+(1, 'admin', 'vonuki@gmail.com', '$2y$10$IqXVr2tNWdNi5dOHVW1YAeFvmY1FBaYs46z6Fgkmnw10DaZDt3oLG', '8iPrHS5L-Dh0Ve0sTf30u8DpDtP1ifPi', 1561662050, NULL, NULL, '194.246.46.15', 1561641238, 1561641238, 0, 1561753050);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Clients`
+--
+ALTER TABLE `Clients`
+  ADD PRIMARY KEY (`idClient`), ADD KEY `idPerson` (`idPerson`);
+
+--
+-- Indexes for table `Items`
+--
+ALTER TABLE `Items`
+  ADD PRIMARY KEY (`idItem`), ADD KEY `idQueue` (`idQueue`), ADD KEY `idClient` (`idClient`);
+
+--
+-- Indexes for table `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexes for table `Owners`
+--
+ALTER TABLE `Owners`
+  ADD PRIMARY KEY (`idOwner`), ADD KEY `idPerson` (`idPerson`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `Queues`
+--
+ALTER TABLE `Queues`
+  ADD PRIMARY KEY (`idQueue`), ADD KEY `idOwner` (`idOwner`);
+
+--
+-- Indexes for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `account_unique` (`provider`,`client_id`), ADD UNIQUE KEY `account_unique_code` (`code`), ADD KEY `fk_user_account` (`user_id`);
+
+--
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD UNIQUE KEY `token_unique` (`user_id`,`code`,`type`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `user_unique_username` (`username`), ADD UNIQUE KEY `user_unique_email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Clients`
+--
+ALTER TABLE `Clients`
+  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Items`
+--
+ALTER TABLE `Items`
+  MODIFY `idItem` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Owners`
+--
+ALTER TABLE `Owners`
+  MODIFY `idOwner` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Queues`
+--
+ALTER TABLE `Queues`
+  MODIFY `idQueue` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `social_account`
+--
+ALTER TABLE `social_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Clients`
+--
+ALTER TABLE `Clients`
+ADD CONSTRAINT `Clients_ibfk_1` FOREIGN KEY (`idPerson`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Items`
+--
+ALTER TABLE `Items`
+ADD CONSTRAINT `Items_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `Clients` (`idClient`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `Items_ibfk_2` FOREIGN KEY (`idQueue`) REFERENCES `Queues` (`idQueue`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Owners`
+--
+ALTER TABLE `Owners`
+ADD CONSTRAINT `Owners_ibfk_1` FOREIGN KEY (`idPerson`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+ADD CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Queues`
+--
+ALTER TABLE `Queues`
+ADD CONSTRAINT `Queues_ibfk_1` FOREIGN KEY (`idOwner`) REFERENCES `Owners` (`idOwner`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `social_account`
+--
+ALTER TABLE `social_account`
+ADD CONSTRAINT `fk_user_account` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `token`
+--
+ALTER TABLE `token`
+ADD CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
