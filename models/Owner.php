2d42f704701f0139c0e4d65df1e552dbac470863
vonuki <vonuki@gmail.com>
2019-07-01 11:11:45 -0400

    Add standart names of modules

---
+++ models/Owner.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Owner".
+ *
+ * @property int $idOwner
+ * @property string $Description
+ * @property int $idPerson
+ * @property int $Status
+ *
+ * @property User $person
+ * @property Queue[] $queues
+ */
+class Owner extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'Owner';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['Description', 'idPerson'], 'required'],
+            [['idPerson', 'Status'], 'integer'],
+            [['Description'], 'string', 'max' => 50],
+            [['idPerson'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['idPerson' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'idOwner' => 'Id Owner',
+            'Description' => 'Description',
+            'idPerson' => 'Id Person',
+            'Status' => 'Status',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPerson()
+    {
+        return $this->hasOne(User::className(), ['id' => 'idPerson']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getQueues()
+    {
+        return $this->hasMany(Queue::className(), ['idOwner' => 'idOwner']);
+    }
+}
