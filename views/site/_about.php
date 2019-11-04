b5795c24d80724f64e1d14da3db9d05c07e0d700
Vonuki <vonuki@gmail.com>
2019-11-04 10:09:17 +0300

    Not using

--- views/site/_about.php
+++ views/site/_about.php
@@ -9,7 +9,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <span><?=Yii::t('lg_common', 'This is test service with allow you to generate simple queue and mange it.') ?> </span> <br>
         <span><?=Yii::t('lg_common', 'Clients have possibility to get in queue and have updates on items changes.') ?> </span>
